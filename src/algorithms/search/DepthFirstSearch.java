8fd84a5d157ef21417ba1f65764591aad5b3638f
Yael <Yael@nitzanim.tech>
2023-04-06 19:44:47 +0300

    I created the DepthFirstSearch class, but I haven't completed the functions yet.

---
+++ src/algorithms/search/DepthFirstSearch.java
@@ -0,0 +1,23 @@
+package algorithms.search;
+
+public class DepthFirstSearch implements ISearchingAlgorithm{
+    @Override
+    public String getNumberOfNodesEvaluated() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return "DepthFirstSearch";
+    }
+
+    @Override
+    public Solution solve(ISearchable domain) {
+        return null;
+    }
+
+    @Override
+    public void search() {
+
+    }
+}
